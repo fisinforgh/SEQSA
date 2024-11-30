322024ac2fd65c8058c044a13039ddc9cd7e1e48
fisinforgh <43649178+fisinforgh@users.noreply.github.com>
2024-11-30 14:35:20 -0500

    Add files via upload

---
+++ SoftEduQSA/SoftEduQSALinkDef.h
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+// NOTICE OF COPYRIGHT                                                         //
+//                                                                             //
+// Copyright (C) 2023                                                          //
+//                                                                             //
+// Authors:								       //
+//   [1] Andrés Felipe García Cárdenas*					       //
+//   [2] Julian Andrés Salamanca Bernal**                                      //
+//   [3] Diego Julián Rodríguez-Patarroyo***				       //         			           
+//                                                                             //
+//   [1] andrfgarciac@udistrital.edu.co (Licenciada Universidad Distrital)     //
+//   [2] jasalamanca@udistrital.edu.co (profesor Universidad Distrital)        //
+//   [3] djrodriguezp@udistrital.edu.co (profesor Universidad Distrital)       //
+//									       //
+//  *,** Grupo de Física e Informática (FISINFOR)		               //
+//  *** Grupo de Laboratorio de Fuentes Alternas de Energía (LIFAE)	       //
+//  *,**,*** Universidad Distrital Francisco José de Caldas (Bogotá, Colombia) //	
+//                                                                             //
+// Web page:								       //
+//   https://github.com/fisinforgh/SoftEduQSA                                  //
+//                                                                             //
+// This program is free software; you can redistribute it and/or modify        //
+// it under the terms of the GNU General Public License as published by        //
+// the Free Software Foundation; either version 2 of the License, or           //
+// (at your option) any later version.                                         //
+//                                                                             //
+// This program is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of              //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               //
+// GNU General Public License for more details:                                //
+//                                                                             //
+//               http://www.gnu.org/copyleft/gpl.html                          //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+#pragma link C++ class MyMainFrame;
